8f588e4ceae9efbd70037648298a4d62e3c43301
Jerry Ryle <jerry@ryle.io>
2023-06-12 15:01:20 -0700

    Added ability to parse ETrade's weird string time format.

--- pkg/etradelib/responses/etrade_time.go
+++ pkg/etradelib/responses/etrade_time.go
@@ -2,6 +2,10 @@ package responses
 
 import (
 	"encoding/xml"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -9,15 +13,49 @@ type ETradeTime struct {
 	time.Time
 }
 
-func (c *ETradeTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	var v int64
-	if err := d.DecodeElement(&v, &start); err != nil {
+func (et *ETradeTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var (
+		v   string
+		err error
+	)
+	err = d.DecodeElement(&v, &start)
+	if err != nil {
 		return err
 	}
-	c.Time = time.Unix(v, 0)
-	return nil
+
+	// Try parsing as Unix timestamp first
+	var unixTime int64
+	if unixTime, err = strconv.ParseInt(v, 10, 64); err == nil {
+		et.Time = time.Unix(unixTime, 0)
+		return nil
+	}
+
+	// If that fails, try parsing as the date-time string that ETrade uses.
+	components := strings.Split(v, " ")
+	if len(components) < 2 {
+		return errors.New(fmt.Sprintf("invalid date format: %s", v))
+	}
+	var location *time.Location
+	switch components[1] {
+	case "EDT", "EST":
+		location, err = time.LoadLocation("America/New_York")
+		if err != nil {
+			return err
+		}
+	default:
+		return errors.New(fmt.Sprintf("unknown timezone in date: %s, (%s is not known)", v, components[1]))
+	}
+
+	var parsedTime time.Time
+	if parsedTime, err = time.ParseInLocation("15:04:05 MST 01-02-2006", v, location); err == nil {
+		et.Time = parsedTime
+		return nil
+	}
+
+	// If that fails, return the parse error
+	return err
 }
 
-func (c *ETradeTime) GetTime() time.Time {
-	return c.Time
+func (et *ETradeTime) GetTime() time.Time {
+	return et.Time
 }
--- pkg/etradelib/responses/etrade_time_test.go
+++ pkg/etradelib/responses/etrade_time_test.go
@@ -19,7 +19,7 @@ func TestETradeTime_UnmarshalXML(t *testing.T) {
 		{
 			name: "Valid Unix timestamp",
 			args: args{
-				xmlData: `<root><unix_epoch>1641582247</unix_epoch></root>`,
+				xmlData: `<root><time>1641582247</time></root>`,
 			},
 			expectErr: false,
 			expect:    parseTimeOrPanic("2022-01-07T19:04:07Z"),
@@ -27,7 +27,7 @@ func TestETradeTime_UnmarshalXML(t *testing.T) {
 		{
 			name: "Valid Zeroed Unix timestamp",
 			args: args{
-				xmlData: `<root><unix_epoch>0</unix_epoch></root>`,
+				xmlData: `<root><time>0</time></root>`,
 			},
 			expectErr: false,
 			expect:    parseTimeOrPanic("1970-01-01T00:00:00Z"),
@@ -35,7 +35,37 @@ func TestETradeTime_UnmarshalXML(t *testing.T) {
 		{
 			name: "Invalid Unix timestamp",
 			args: args{
-				xmlData: `<root><unix_epoch>invalid</unix_epoch></root>`,
+				xmlData: `<root><time>invalid</time></root>`,
+			},
+			expectErr: true,
+		},
+		{
+			name: "Valid ETrade string timestamp (EDT)",
+			args: args{
+				xmlData: `<root><time>16:00:00 EDT 06-20-2012</time></root>`,
+			},
+			expectErr: false,
+			expect:    parseTimeOrPanic("2012-06-20T20:00:00Z"),
+		},
+		{
+			name: "Valid ETrade string timestamp (EST)",
+			args: args{
+				xmlData: `<root><time>16:00:00 EST 05-20-2012</time></root>`,
+			},
+			expectErr: false,
+			expect:    parseTimeOrPanic("2012-05-20T21:00:00Z"),
+		},
+		{
+			name: "Invalid ETrade string timestamp",
+			args: args{
+				xmlData: `<root><time>16:00:00EST 05-20-2012</time></root>`,
+			},
+			expectErr: true,
+		},
+		{
+			name: "Invalid Timezone",
+			args: args{
+				xmlData: `<root><time>16:00:00 MOO 05-20-2012</time></root>`,
 			},
 			expectErr: true,
 		},
@@ -43,7 +73,7 @@ func TestETradeTime_UnmarshalXML(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			var wrapper struct {
-				Time ETradeTime `xml:"unix_epoch"`
+				Time ETradeTime `xml:"time"`
 			}
 			if err := xml.Unmarshal([]byte(tt.args.xmlData), &wrapper); (err != nil) != tt.expectErr {
 				t.Errorf("ETradeTime.UnmarshalXML() error = %v, expectErr %v", err, tt.expectErr)
