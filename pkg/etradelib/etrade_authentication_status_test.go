6df9c00762bf8bcaffb4ddc16f09fa0188a0bc59
Jerry Ryle <jerry@ryle.io>
2023-07-03 14:40:13 -0700

    Added missing tests.

--- pkg/etradelib/etrade_authentication_status_test.go
+++ pkg/etradelib/etrade_authentication_status_test.go
@@ -3,7 +3,6 @@ package etradelib
 import (
 	"github.com/jerryryle/etrade-cli/pkg/etradelib/jsonmap"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -30,15 +29,54 @@ func TestCreateETradeAuthenticationStatus(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "Creates Success Status",
+			testJson: `
+{
+  "status": "success"
+}`,
+			expectErr: false,
+			expectValue: &eTradeAuthenticationStatus{
+				authorizationUrl: "",
+				jsonMap: jsonmap.JsonMap{
+					"status": "success",
+				},
+			},
+		},
+		{
+			name: "Fails Without Status",
+			testJson: `
+{
+  "authorizationUrl": "test url"
+}`,
+			expectErr:   true,
+			expectValue: nil,
+		},
+		{
+			name: "Authorize Status Fails Without Url",
+			testJson: `
+{
+  "status": "authorize"
+}`,
+			expectErr:   true,
+			expectValue: nil,
+		},
+		{
+			name: "Fails On Bad JSON",
+			testJson: `
+{
+  "status": 
+}`,
+			expectErr:   true,
+			expectValue: nil,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(
 			tt.name, func(t *testing.T) {
-				responseMap, err := NewNormalizedJsonMap([]byte(tt.testJson))
-				require.Nil(t, err)
 				// Call the Method Under Test
-				actualValue, err := CreateETradeAuthenticationStatus(responseMap)
+				actualValue, err := CreateETradeAuthenticationStatusFromResponse([]byte(tt.testJson))
 				if tt.expectErr {
 					assert.Error(t, err)
 				} else {
--- pkg/etradelib/etrade_status_test.go
+++ pkg/etradelib/etrade_status_test.go
@@ -1,1 +1,110 @@
 package etradelib
+
+import (
+	"github.com/jerryryle/etrade-cli/pkg/etradelib/jsonmap"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreateETradeStatus(t *testing.T) {
+	tests := []struct {
+		name        string
+		testJson    string
+		expectErr   bool
+		expectValue ETradeStatus
+	}{
+		{
+			name: "Creates Success Status",
+			testJson: `
+{
+  "status": "success"
+}`,
+			expectErr: false,
+			expectValue: &eTradeStatus{
+				isSuccess:    true,
+				errorMessage: "",
+				jsonMap: jsonmap.JsonMap{
+					"status": "success",
+				},
+			},
+		},
+		{
+			name: "Creates Error Status",
+			testJson: `
+{
+  "status": "error",
+  "error": "test error message"
+}`,
+			expectErr: false,
+			expectValue: &eTradeStatus{
+				isSuccess:    false,
+				errorMessage: "test error message",
+				jsonMap: jsonmap.JsonMap{
+					"status": "error",
+					"error":  "test error message",
+				},
+			},
+		},
+		{
+			name: "Fails Without Status",
+			testJson: `
+{
+  "test key": "test value"
+}`,
+			expectErr:   true,
+			expectValue: nil,
+		},
+		{
+			name: "Error Status Fails Without Error Message",
+			testJson: `
+{
+  "status": "error"
+}`,
+			expectErr:   true,
+			expectValue: nil,
+		},
+		{
+			name: "Fails On Bad JSON",
+			testJson: `
+{
+  "status": 
+}`,
+			expectErr:   true,
+			expectValue: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				// Call the Method Under Test
+				actualValue, err := CreateETradeStatusFromResponse([]byte(tt.testJson))
+				if tt.expectErr {
+					assert.Error(t, err)
+				} else {
+					assert.Nil(t, err)
+				}
+				assert.Equal(t, tt.expectValue, actualValue)
+			},
+		)
+	}
+}
+
+func TestETradeStatus_AsJsonMap(t *testing.T) {
+	testObject := &eTradeStatus{
+		isSuccess:    false,
+		errorMessage: "test error message",
+		jsonMap: jsonmap.JsonMap{
+			"status": "error",
+			"error":  "test error message",
+		},
+	}
+
+	expectedValue := jsonmap.JsonMap{
+		"status": "error",
+		"error":  "test error message",
+	}
+
+	actualValue := testObject.AsJsonMap()
+	assert.Equal(t, expectedValue, actualValue)
+}
