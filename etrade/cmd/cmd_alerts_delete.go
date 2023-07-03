e4ab652600e509be7042c6e3824f73d29d4a320f
Jerry Ryle <jerry@ryle.io>
2023-07-03 16:08:51 -0700

    DeleteAlerts command now returns status instead of the updated list of alerts.

--- etrade/cmd/cmd_alerts_delete.go
+++ etrade/cmd/cmd_alerts_delete.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/jerryryle/etrade-cli/pkg/etradelib/jsonmap"
 	"github.com/spf13/cobra"
+	"strings"
 )
 
 type CommandAlertsDelete struct {
@@ -17,11 +20,38 @@ func (c *CommandAlertsDelete) Command() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			alertIds := args
 			if response, err := DeleteAlerts(c.Context.Client, alertIds); err == nil {
-				return c.Context.Renderer.Render(response, alertListDescriptor)
+				return c.Context.Renderer.Render(response, deleteAlertsDescriptor)
 			} else {
 				return err
 			}
 		},
 	}
 	return cmd
+}
+
+var deleteAlertsDescriptor = []RenderDescriptor{
+	{
+		ObjectPath: "",
+		Values: []RenderValue{
+			{Header: "Status", Path: ".status"},
+			{Header: "Error Message", Path: ".error"},
+			{
+				Header: "Failed to Delete", Path: ".failedAlerts", Transformer: transformAlertList,
+			},
+		},
+		DefaultValue: "",
+		SpaceAfter:   false,
+	},
+}
+
+func transformAlertList(value interface{}) interface{} {
+	if alertList, ok := value.(jsonmap.JsonSlice); ok {
+		alertStringList := make([]string, len(alertList))
+		for i, v := range alertList {
+			alertStringList[i] = fmt.Sprintf("%d", v)
+		}
+		return strings.Join(alertStringList, ", ")
+	} else {
+		return value
+	}
 }
--- etrade/cmd/fn_delete_alerts.go
+++ etrade/cmd/fn_delete_alerts.go
@@ -1,28 +1,19 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/jerryryle/etrade-cli/pkg/etradelib"
 	"github.com/jerryryle/etrade-cli/pkg/etradelib/client"
-	"github.com/jerryryle/etrade-cli/pkg/etradelib/client/constants"
 	"github.com/jerryryle/etrade-cli/pkg/etradelib/jsonmap"
 )
 
 func DeleteAlerts(eTradeClient client.ETradeClient, alertIds []string) (jsonmap.JsonMap, error) {
-	//TODO: Make this return status instead of listing alerts
-	_, err := eTradeClient.DeleteAlerts(alertIds)
-	if err != nil {
-		return nil, fmt.Errorf("requested Alert Id(s) may not exist (%w)", err)
-	}
-	response, err := eTradeClient.ListAlerts(
-		constants.AlertsMaxCount, constants.AlertCategoryNil, constants.AlertStatusNil, constants.SortOrderNil, "",
-	)
+	response, err := eTradeClient.DeleteAlerts(alertIds)
 	if err != nil {
 		return nil, err
 	}
-	alertsList, err := etradelib.CreateETradeAlertListFromResponse(response)
+	deleteAlerts, err := etradelib.CreateETradeDeleteAlertsFromResponse(response)
 	if err != nil {
 		return nil, err
 	}
-	return alertsList.AsJsonMap(), nil
+	return deleteAlerts.AsJsonMap(), nil
 }
